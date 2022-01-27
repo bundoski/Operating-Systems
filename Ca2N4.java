d92d49fa6ee0078d168eca51c9d4d931dbc61b01
sadastro <93097556+sadastro@users.noreply.github.com>
2022-01-27 01:14:25 +0100

    Add files via upload

---
+++ Ca2N4.java
@@ -0,0 +1,47 @@
+package Auditoriski.AUD4Synchronization.Ispitni;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Ca2N4 {
+
+    private static Semaphore caHere = new Semaphore(0);
+    private static Semaphore nHere = new Semaphore(4);
+    private static Semaphore canBond = new Semaphore(0);
+    private static Semaphore canExit = new Semaphore(0);
+
+    private static int totalAtoms = 0;
+
+    private static Lock lock = new ReentrantLock();
+
+    @Override
+    public void execute() throws InterruptedException{
+
+        caHere.acquire();
+
+        lock.lock();
+        totalAtoms++;
+        if(totalAtoms == 6){
+            // notify other atoms that are waiting to bond
+            canBond.release(6);
+        }
+        lock.unlock();
+
+        canBond.acquire();
+        state.bond();
+        // empty
+
+        lock.lock();
+        totalAtoms--;
+        if(totalAtoms==0){
+            state.validate();
+            canExit.release(6);
+            // ili canBond.release(), ili prazno..
+
+        }
+        lock.unlock();
+        canExit.acquire(1);
+        caHere.release(1);   // ako ima opcija da go nema ova, to e tocen odgovor...
+    }
+}
