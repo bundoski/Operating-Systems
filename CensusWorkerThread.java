5270a2e310da021d38f2f7bc6516228acd4b49f3
sadastro <93097556+sadastro@users.noreply.github.com>
2022-01-27 03:53:13 +0100

    Update CensusWorkerThread.java

--- CensusWorkerThread.java
+++ CensusWorkerThread.java
@@ -1,9 +1,8 @@
 package Networking.tcp.censusIspitna.server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class CensusWorkerThread extends Thread {
 
@@ -33,14 +32,44 @@ public class CensusWorkerThread extends Thread {
 
         // ispisuva Vnesi index kaj kliento, pa ceka informacija od kliento.
         try {
-            dos.writeUTF("Vnesi index: ");
+            dos.writeUTF("READY"); // prajchame ready pri konekcija
             dos.flush();
-            String index = dis.readUTF(); // na ovaj nacin cekame string sho kliento bi ni go pratil..
-            dos.writeUTF("Vnesi ime i prezime");
-            dos.flush();
-            String nameSurname = dis.readUTF();
+            // send data as MAIL FROM:<emailFrom> ...
+            String mailFrom = dis.readUTF(); // na ovaj nacin cekame string sho kliento bi ni go pratil..
+            try {
+                if (mailFrom.contains("@")) {
+                    dos.writeUTF("250");
+                }
+            } catch (IOException e){
+                System.out.println("That is not an email..");
+            }
+            System.out.printf("MAIL FROM: %s", mailFrom);
+            System.out.println();
+
+            // write MAIL TO:<emailTo> message.
+            String mailTo = dis.readUTF();
+            try{
+                if(mailTo.contains("@")){
+                    dos.writeUTF("THANK YOU");
+                }
+            } catch (IOException e){
+                System.out.println("That is not an email..");
+            }
+            System.out.printf("MAIL TO: %s", mailTo);
+            System.out.println();
+            String subject = dis.readUTF();
+            try{
+                if(subject.length()<=200){
+                    dos.writeUTF("OK");
+                }
+            } catch (IOException e){
+                System.out.println("Subject is longer than 200 characters");
+            }
+            System.out.printf("SUBJECT: %s", subject);
+            String totaldata = mailFrom + " " + mailTo + " " + subject;
+            dos.writeUTF("RECEIVED: " + totaldata.length());
             // shtom dobijame index, ime i prezime , gi zapisuvame vo fajl.
-            censusWriter.addRecordToFile(index, nameSurname); // gi dodavame iminjata i indekso itn
+            censusWriter.addRecordToFile(mailFrom, mailTo, subject); // gi dodavame iminjata i indekso itn
 
         } catch (IOException exception) {
             exception.printStackTrace();
