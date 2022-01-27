4a44ea6d7b45acf9a2f005593818195dc335797e
sadastro <93097556+sadastro@users.noreply.github.com>
2022-01-27 02:13:18 +0100

    Update Ca2N4.java

--- Ca2N4.java
+++ Ca2N4.java
@@ -1,13 +1,14 @@
 package Auditoriski.AUD4Synchronization.Ispitni;
 
+
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Ca2N4 {
 
-    private static Semaphore caHere = new Semaphore(0);  // ili 0 ili 2..
-    private static Semaphore nHere = new Semaphore(4);
+    private static Semaphore caHere = new Semaphore(0);
+    private static Semaphore nHere = new Semaphore(0);
     private static Semaphore canBond = new Semaphore(0);
     private static Semaphore canExit = new Semaphore(0);
 
@@ -18,13 +19,13 @@ public class Ca2N4 {
     @Override
     public void execute() throws InterruptedException{
 
-        caHere.acquire(1);
+        caHere.acquire();
 
         lock.lock();
         totalAtoms++;
         if(totalAtoms == 6){
             // notify other atoms that are waiting to bond
-            canBond.release(6);
+            canBond.release(5);
         }
         lock.unlock();
 
@@ -36,7 +37,7 @@ public class Ca2N4 {
         totalAtoms--;
         if(totalAtoms==0){
             state.validate();
-            canExit.release(6);
+            canExit.release(2);
             // ili canBond.release(), ili prazno..
 
         }
