22d076e11adb02fe90729ea8e80018e284349ffc
sadastro <93097556+sadastro@users.noreply.github.com>
2022-01-27 03:53:52 +0100

    Update CensusClient.java

--- CensusClient.java
+++ CensusClient.java
@@ -1,14 +1,13 @@
-package Networking.tcp.censusIspitna.client;
 
 import Networking.tcp.censusIspitna.server.CensusServer;
 import Networking.tcp.client.TCPclient;
-
 import java.io.DataInput;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
+
 
 public class CensusClient extends Thread{
 
@@ -34,20 +33,21 @@ public class CensusClient extends Thread{
             this.dos = new DataOutputStream(this.socket.getOutputStream());
 
             // ovde dobivame Vnesi index isprateno od strana na serverot, to se pecati kaj ekrano na kliento..
-            String indexCmd = dis.readUTF();
-            System.out.println(indexCmd);
-            // vnesuvame nash index
-            String index = scanner.nextLine();
-            this.dos.writeUTF(index); // mu go prajchame vnesenio index..
-            this.dos.flush();
-            // dobivame poraka vnesi ime i prezime;
-            String nameSurnameCmd = dis.readUTF();
-            System.out.println(nameSurnameCmd);
-            // ovde gi citame imeto i prezimeto
-            String nameSurname = scanner.nextLine();
-            this.dos.writeUTF(nameSurname);  // go prajchame ...
-            this.dos.flush();
-
+            while(true) {
+                String indexCmd = dis.readUTF();
+                System.out.println(indexCmd);
+                // vnesuvame nash index
+                String index = scanner.nextLine();
+                this.dos.writeUTF(index); // mu go prajchame vnesenio index..
+                this.dos.flush();
+                // dobivame poraka vnesi ime i prezime;
+                String nameSurnameCmd = dis.readUTF();
+                System.out.println(nameSurnameCmd);
+                // ovde gi citame imeto i prezimeto
+                String nameSurname = scanner.nextLine();
+                this.dos.writeUTF(nameSurname);  // go prajchame ...
+                this.dos.flush();
+            }
 
         } catch (IOException exception) {
             exception.printStackTrace();
@@ -62,7 +62,7 @@ public class CensusClient extends Thread{
     public static void main(String[] args) {
         // prajme test na kliento..
 
-        CensusClient client = new CensusClient("localhost", 9999);
+        CensusClient client = new CensusClient("localhost", 5432);
         client.start();
     }
 }
